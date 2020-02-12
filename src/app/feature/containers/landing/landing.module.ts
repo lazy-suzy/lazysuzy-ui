95cd3dbfa13ab8456c93e0f8ae9bc1c1f07b98fd
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-02-12 19:35:26 +0530

    search ui

--- src/app/feature/containers/landing/landing.module.ts
+++ src/app/feature/containers/landing/landing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
+import { MatIconModule } from '@angular/material/icon';
 
 const DECLARATIONS = [LandingComponent];
 
-const MODULES = [CommonModule];
+const MODULES = [CommonModule, MatIconModule];
 
 @NgModule({
   declarations: [...DECLARATIONS],
   imports: [...MODULES],
-  exports: [...DECLARATIONS, ...MODULES],
+  exports: [...DECLARATIONS, ...MODULES]
 })
 export class LandingModule {}
