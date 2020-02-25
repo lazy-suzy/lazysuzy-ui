083865cfcfa1d64ff01b41b7ba1792dc7bcd0b6d
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-02-25 12:30:10 +0530

    search product fixes

--- src/app/feature/components/search-product/search-product.component.ts
+++ src/app/feature/components/search-product/search-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDetailsComponent } from './../product-details/product-details.component';
 import { ISearchProduct } from './../../../shared/models';
 
 @Component({
@@ -10,9 +12,11 @@ export class SearchProductComponent implements OnInit {
   @Input() product: ISearchProduct;
   starIcons: string[] = new Array();
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.product.main_product_images =
+      '//www.lazysuzy.com' + this.product.main_product_images;
     this.setRating();
   }
 
@@ -30,5 +34,17 @@ export class SearchProductComponent implements OnInit {
         this.starIcons.push('star_outline');
       }
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ProductDetailsComponent, {
+      width: '80%',
+      height: '80%',
+      data: { sku: this.product.product_sku }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log('The dialog was closed');
+    });
   }
 }
