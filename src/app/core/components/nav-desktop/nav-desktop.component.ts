3b7e6ac72d2580b1b1c8a894e855973e5d6cc928
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-03-02 23:28:16 +0530

    mobile nav section with api

--- src/app/core/components/nav-desktop/nav-desktop.component.ts
+++ src/app/core/components/nav-desktop/nav-desktop.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MOCK_ACCOUNT_ACTIONS, MOCK_DEPARTMENTS } from './../../../mocks';
 import { ICategory, IDepartment } from './../../../shared/models';
+import { Router,NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-desktop',
@@ -12,4 +15,22 @@ export class NavDesktopComponent {
   logoPath: string = 'assets/images/color_logo_transparent.png';
   departments: IDepartment[] = MOCK_DEPARTMENTS;
   accountActions: ICategory[] = MOCK_ACCOUNT_ACTIONS;
+  presentRoute:string;
+  notHome: Boolean;
+  checkHomeRoute: Subscription;
+  constructor(
+     private router: Router, private location: Location
+  ) {
+    this.checkHomeRoute = router.events.subscribe(val => {
+      this.notHome = location.path() !== '';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.checkHomeRoute.unsubscribe();
+  }
+
+  ngOnInit(): void {
+  
+  }
 }
--- src/app/core/components/nav-mobile/nav-mobile.component.ts
+++ src/app/core/components/nav-mobile/nav-mobile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MOCK_ACCOUNT_ACTIONS, MOCK_DEPARTMENTS } from 'src/app/mocks';
-import { ICategory, IDepartment } from 'src/app/shared/models';
+import { ApiService } from './../../../shared/services';
+import { Router} from '@angular/router';
+import { IAllDepartment } from '../../../shared/models/all-department.interface';
 
 @Component({
   selector: 'app-nav-mobile',
@@ -9,6 +11,36 @@ import { ICategory, IDepartment } from 'src/app/shared/models';
 })
 export class NavMobileComponent {
   logoPath: string = 'assets/images/color_logo_transparent.png';
-  departments: IDepartment[] = MOCK_DEPARTMENTS;
-  accountActions: ICategory[] = MOCK_ACCOUNT_ACTIONS;
+  departments: IAllDepartment[];
+  selectedIndex: number = null;
+  menuVisible = false;
+  mobileMenuContainer;
+  showDepartment;
+
+  constructor(private apiService: ApiService, private router: Router) {
+    this.getDepartments();
+  }
+
+  setIndex(index: number) {
+      this.selectedIndex = index;
+  }
+
+  getDepartments() {
+    this.apiService.getAllDepartments()
+    .subscribe((payload: any) => {
+      this.departments = payload.all_departments;
+    });
+  }
+
+  showMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  showDepartmentChild(dept) {
+    if(dept === this.showDepartment) {
+      this.showDepartment = undefined;
+      return;
+    }
+    this.showDepartment = dept;
+  }
 }
