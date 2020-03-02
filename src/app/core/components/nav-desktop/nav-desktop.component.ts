930cdcd61a9c83b0cbd65c85127aa8f4ca3fdeb3
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-03-02 23:09:15 +0530

    search in header and footer sticky

--- src/app/core/components/nav-desktop/nav-desktop.component.ts
+++ src/app/core/components/nav-desktop/nav-desktop.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MOCK_ACCOUNT_ACTIONS, MOCK_DEPARTMENTS } from './../../../mocks';
 import { ICategory, IDepartment } from './../../../shared/models';
+import { ActivatedRoute, Router,NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-desktop',
@@ -12,4 +15,33 @@ export class NavDesktopComponent {
   logoPath: string = 'assets/images/color_logo_transparent.png';
   departments: IDepartment[] = MOCK_DEPARTMENTS;
   accountActions: ICategory[] = MOCK_ACCOUNT_ACTIONS;
+  presentRoute;
+  notHome: Boolean;
+  checkHomeRoute: Subscription;
+  constructor(
+    private activeRoute: ActivatedRoute, private router: Router, private location: Location
+  ) {
+    this.checkHomeRoute = router.events.subscribe(val => {
+      this.notHome = location.path() !== '';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.checkHomeRoute.unsubscribe();
+  }
+
+  ngOnInit(): void {
+
+  //   this.presentRoute = this.router.events.subscribe((val: NavigationEnd) => {
+  //     if (val instanceof NavigationEnd) {
+  //       // Hide loading indicator
+  //     }
+  //   });
+  //   console.log(this.presentRoute);
+  //   if( this.presentRoute === '/' ){
+  //     this.isHome = true;
+  //   } else {
+  //     this.isHome = false;
+  //   }
+  }
 }
--- src/app/feature/containers/categories/categories.module.ts
+++ src/app/feature/containers/categories/categories.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories.component';
 
 const DECLARATIONS = [CategoriesComponent];
 
-const MODULES = [CommonModule];
+const MODULES = [CommonModule, RouterModule];
 
 @NgModule({
   declarations: [...DECLARATIONS],
