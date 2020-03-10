572e9a8d9b111e8134ce1db0fa6ba575a559ede5
Mobilyte <mobilyte.apps@gmail.com>
2020-03-11 04:43:42 +0530

    required_changes_done

---
+++ src/app/core/components/auth/auth.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular-6-social-login';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.less']
+})
+export class AuthComponent implements OnInit {
+
+  constructor(  private socialAuthService: AuthService) { }
+
+  ngOnInit() {
+  }
+  public socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    if(socialPlatform == "facebook"){
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }else if(socialPlatform == "google"){
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    } else{ return; }
+    
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform+" sign in data : " , userData);
+      }
+    );
+  }
+
+}
---
+++ src/app/core/components/auth/auth.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider,} from "angular-6-social-login";
+import { FormsModule } from '@angular/forms';
+
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("840582405192-fkr4bq3fp5q5doe386kkfikeie1ggn0p.apps.googleusercontent.com")
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider("545201916109098")
+        },
+      ]
+  );
+  return config;
+}
+
+
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    SocialLoginModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
+  ],
+  exports: [AuthComponent, CommonModule, SocialLoginModule],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+    ],
+})
+export class AuthModule { }
