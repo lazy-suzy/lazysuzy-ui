7ccc98a76d6a0b077433ea48a7c8464716c475bf
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-03-13 21:28:57 +0530

    auth module updated

--- src/app/core/components/auth/auth.component.ts
+++ src/app/core/components/auth/auth.component.ts
@@ -4,7 +4,7 @@ import {
   FacebookLoginProvider,
   GoogleLoginProvider
 } from 'angular-6-social-login';
-import {CookieService} from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from 'src/app/shared/services';
 
 @Component({
@@ -13,13 +13,17 @@ import { ApiService } from 'src/app/shared/services';
   styleUrls: ['./auth.component.less']
 })
 export class AuthComponent implements OnInit {
-  @ViewChild('closeLoginModal') closeLoginModal: ElementRef;
+  @ViewChild('closeLoginModal', { static: false }) closeLoginModal: ElementRef;
 
   userCookie: string;
   user: any;
-  constructor(  private socialAuthService: AuthService, private apiService: ApiService, private cookie: CookieService) { }
+  constructor(
+    private socialAuthService: AuthService,
+    private apiService: ApiService,
+    private cookie: CookieService
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.fetchUser();
   }
 
@@ -30,44 +34,39 @@ export class AuthComponent implements OnInit {
     }
   }
 
-  public socialSignIn(socialPlatform : string) {
+  public socialSignIn(socialPlatform: string) {
     let socialPlatformProvider;
-    if(socialPlatform == "facebook"){
+    if (socialPlatform == 'facebook') {
       socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    }else if(socialPlatform == "google"){
+    } else if (socialPlatform == 'google') {
       socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    } else{ return; }
-    
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-      }
-    );
+    } else {
+      return;
+    }
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(userData => {});
   }
 
   login(email, password) {
     const formData: any = new FormData();
     formData.append('email', email);
     formData.append('password', password);
-    this.apiService
-      .login(formData)
-      .subscribe((payload: any) => {
-        this.cookie.set('token', payload.success.token);
-        localStorage.setItem('user', JSON.stringify(payload.user));
-        this.closeLoginModal.nativeElement.click();
-        this.fetchUser();
-      });
+    this.apiService.login(formData).subscribe((payload: any) => {
+      this.cookie.set('token', payload.success.token);
+      localStorage.setItem('user', JSON.stringify(payload.user));
+      this.closeLoginModal.nativeElement.click();
+      this.fetchUser();
+    });
   }
 
-  signup(name,email, password) {
+  signup(name, email, password) {
     var formData: any = new FormData();
     formData.append('name', name);
     formData.append('email', email);
     formData.append('password', password);
-    this.apiService
-      .signup(formData)
-      .subscribe((payload: any) => {
-        console.log(payload);
-      });
+    this.apiService.signup(formData).subscribe((payload: any) => {
+      console.log(payload);
+    });
   }
 
   logout() {
