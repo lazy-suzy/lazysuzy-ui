f4a15807a48215a9ff8fc8f1cbba496f3c2db049
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-03-17 15:33:22 +0530

    wishlist code commented

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,10 @@ export const environment = {
   APP_NAME: 'LazySuzy',
   JSON_BASE_HREF: 'assets/resources/',
   API_BASE_HREF: 'http://staging.lazysuzy.com/api/',
-  ES_API_BASE_HREF: 'http://lazysuzy.com:9200/'
+  BASE_HREF: 'http://staging.lazysuzy.com/api',
+  ES_API_BASE_HREF: 'https://www.lazysuzy.com:9200/',
+  API_HREF: 'http://www.lazysuzy.com',
+  LIVE_SITE: false,
+  GOOGLE_LINK: 'http://staging.lazysuzy.com/redirect/google',
+  FACEBOOK_LINK: 'http://staging.lazysuzy.com/redirect/facebook'
 };
