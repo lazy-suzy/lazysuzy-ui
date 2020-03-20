9a0f75ca6b7bc525e404d02ef941dfe54a4a003a
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-03-20 18:59:07 +0530

    enn updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@ export const environment = {
   useLocalJson: false,
   APP_NAME: 'LazySuzy',
   JSON_BASE_HREF: 'assets/resources/',
-  API_BASE_HREF: 'http://staging.lazysuzy.com/api/',
-  BASE_HREF: 'http://staging.lazysuzy.com/api',
+  API_BASE_HREF: 'https://www.lazysuzy.com/api/',
+  BASE_HREF: 'https://www.lazysuzy.com/api',
   ES_API_BASE_HREF: 'https://www.lazysuzy.com:9200/',
-  API_HREF: 'http://staging.lazysuzy.com',
+  API_HREF: 'https://www.lazysuzy.com',
   LIVE_SITE: false,
-  GOOGLE_LINK: 'http://staging.lazysuzy.com/redirect/google',
-  FACEBOOK_LINK: 'http://staging.lazysuzy.com/redirect/facebook'
+  GOOGLE_LINK: 'https://www.lazysuzy.com/redirect/google',
+  FACEBOOK_LINK: 'https://www.lazysuzy.com/redirect/facebook'
 };
