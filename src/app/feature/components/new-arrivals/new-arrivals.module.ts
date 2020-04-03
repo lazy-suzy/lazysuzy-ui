dcb5b96aa0c9fe7b4a9335f8ba0f39e56065c7fe
Ashwath <ashwath.bharadwaj@outlook.com>
2020-04-04 00:37:24 +0530

    Temp commit to test carousal on mobile

--- src/app/feature/components/new-arrivals/new-arrivals.module.ts
+++ src/app/feature/components/new-arrivals/new-arrivals.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewArrivalsComponent } from './new-arrivals.component';
 import { CarouselModule } from 'primeng/carousel';
 import { RouterModule } from '@angular/router';
-
+import { OwlModule } from 'ngx-owl-carousel';
 @NgModule({
   declarations: [NewArrivalsComponent],
-  imports: [CommonModule, CarouselModule, RouterModule],
+  imports: [CommonModule, CarouselModule, RouterModule, OwlModule],
   exports: [NewArrivalsComponent, CarouselModule, CommonModule]
 })
 export class NewArrivalsModule {}
