5b029f141ce11163971e6dbe1bb8e1960b6bdf43
Ashwath <ashwath.bharadwaj@outlook.com>
2020-04-07 09:55:07 +0530

    Carousal fix

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from './../../../shared/services';
-import { Subscription } from 'rxjs';
-import { IProductPayload, IProductsPayload } from './../../../shared/models';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,18 +8,21 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./banner.component.less']
 })
 export class BannerComponent implements OnInit {
-  
-  bannerData: any;
-  isBanner:boolean = true;
 
- constructor(private apiService: ApiService, private router: Router,
-  private activeRoute: ActivatedRoute){}  
- 
+  bannerData: any;
+  isBanner: boolean = true;
+  @Input() isHandset: boolean = false;
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) { }
+
   ngOnInit() {
-    this.apiService.bannerData().subscribe((res)=>{
-    this.bannerData  = res;
+    this.apiService.bannerData().subscribe((res) => {
+      this.bannerData = res;
     })
-    
   }
- 
+
 }
