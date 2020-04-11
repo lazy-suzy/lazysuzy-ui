a3d05ad3a632a9a2b539e58c15e8fce9a745d1a2
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-04-11 11:35:27 +0530

    code recovery for variations

--- src/app/feature/components/product/product.module.ts
+++ src/app/feature/components/product/product.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ProductDetailsComponent } from './../product-details/product-details.component';
 import { ProductComponent } from './product.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { VariationsModule } from '../variations/variations.module';
 
 const DECLARATIONS = [ProductComponent, ProductDetailsComponent];
 
-const MODULES = [CommonModule, MatCardModule, MatIconModule, MatDialogModule, MatButtonModule];
+const MODULES = [
+  CommonModule,
+  MatCardModule,
+  MatIconModule,
+  MatDialogModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  VariationsModule
+];
 
 const COMPONENTS = [ProductDetailsComponent];
 
@@ -17,6 +27,6 @@ const COMPONENTS = [ProductDetailsComponent];
   declarations: [...DECLARATIONS],
   entryComponents: [...COMPONENTS],
   imports: [...MODULES],
-  exports: [...DECLARATIONS, ...MODULES],
+  exports: [...DECLARATIONS, ...MODULES]
 })
 export class ProductModule {}
