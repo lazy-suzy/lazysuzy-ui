5e5603d483978ebe89ca1c905b0d06fc30e01f2d
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-05-07 15:58:09 +0530

    add to cart on sets

--- src/app/feature/components/sets/sets.component.ts
+++ src/app/feature/components/sets/sets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ApiService, UtilsService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-sets',
@@ -9,7 +10,7 @@ export class SetsComponent implements OnInit {
   @Input() sets = [];
   @Input() brand: string;
 
-  constructor() {}
+  constructor(private apiService: ApiService, private utils: UtilsService) {}
 
   ngOnInit() {}
 
@@ -18,5 +19,27 @@ export class SetsComponent implements OnInit {
     if (typeof vglnk) {
       vglnk.open(url, '_blank');
     }
+  }
+  openCartModal(item) {
+    const data = {
+      sku: item.sku,
+      brand: this.brand,
+      image: item.image,
+      name: item.name,
+      price: item.inventory_product_details.price,
+      quantity: 1
+    };
+    const postData = {
+      product_sku: item.sku,
+      count: 1
+    };
+    this.apiService.addCartProduct(postData).subscribe(
+      (payload: any) => {
+        this.utils.openAddToCartDialog(data);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 }
