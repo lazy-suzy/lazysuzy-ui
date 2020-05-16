34e66cd47eb89d403456b2f4ad55b27aac8e9b81
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-05-16 15:21:36 +0530

    about page

--- src/app/core/components/aboutus/aboutus.component.ts
+++ src/app/core/components/aboutus/aboutus.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.less']
+  styleUrls: ['./aboutus.component.less'],
 })
 export class AboutusComponent implements OnInit {
+  logoPath: string = 'assets/image/color_logo_transparent.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
