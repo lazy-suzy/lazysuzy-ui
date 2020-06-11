37d9cf3e98f58aae5fb7b44d024807a46f5eae50
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-06-11 23:15:05 +0530

    admin request updated

--- src/app/modules/admin-dashboard/admin-dashboard.service.ts
+++ src/app/modules/admin-dashboard/admin-dashboard.service.ts
@@ -19,7 +19,7 @@ export class AdminDashboardService {
     page: number
   ): Observable<any> {
     const endpoint = `products/all`;
-    const url = `${env.ADMIN_API_BASE_HREF}${endpoint}?limit=${total}&filters=${filters}&sort_type=${sortType}&pageno=${page}&board-view=true`;
+    const url = `${env.ADMIN_API_BASE_HREF}${endpoint}?limit=${total}&filters=${filters}&sort_type=${sortType}&pageno=${page}&board-view=true&admin=true`;
     return this.httpService.get(url);
   }
   tagImage(data): Observable<any> {
