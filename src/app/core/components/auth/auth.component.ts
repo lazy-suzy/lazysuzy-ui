0d018097a32c08c44332b5783629a9448ff31cc5
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-07-11 21:06:05 +0530

    board list and preview fixes on ipad

--- src/app/core/components/auth/auth.component.ts
+++ src/app/core/components/auth/auth.component.ts
@@ -143,7 +143,6 @@ export class AuthComponent implements OnInit {
       this.cookie.delete('token');
       localStorage.removeItem('user');
       localStorage.removeItem('cart');
-      localStorage.removeItem('hasTooltip');
       this.checkIfGuestIsValid();
     });
   }
