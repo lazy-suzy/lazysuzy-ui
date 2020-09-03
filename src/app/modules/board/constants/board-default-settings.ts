b738c41601ab8d1dbbd79ff11eaeec8826a3cbc4
preferredMS727 <massimiliano.soremic4@gmail.com>
2020-09-04 00:25:00 +0800

    push again

--- src/app/modules/board/constants/board-default-settings.ts
+++
@@ -1,7 +0,0 @@
-import { IBoardSettings } from '../models/board.interface';
-
-export const DefaultBoardSettings: IBoardSettings = {
-    boardHeight: 100,
-    boardWeight: 100,
-    boardColor: 'blue'
-};
