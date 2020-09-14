6632b8f3de2fcadec083d616d93b262d9aa41aeb
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-09-14 09:57:48 +0530

    Updated filters in new product-row

--- src/app/modules/new-products-dashboard/new-product-listing/new-product-listing.component.ts
+++ src/app/modules/new-products-dashboard/new-product-listing/new-product-listing.component.ts
@@ -1,3 +1,4 @@
+import { Color } from './../../../shared/models/products-payload.interface';
 import { tap, take } from "rxjs/operators";
 import { ApiService } from "./../../../shared/services/api/api.service";
 import { Component, OnInit } from "@angular/core";
@@ -18,7 +19,7 @@ export class NewProductListingComponent implements OnInit {
   materials: any = [];
   spinner = "assets/image/spinner.gif";
   page = 1;
-
+  filters:any;
   //For Infinite Scroll
   throttle = 300;
   scrollDistance = 1;
@@ -37,9 +38,10 @@ export class NewProductListingComponent implements OnInit {
       .pipe(
         take(1) // take only one result and then unsubscribe
       )
-      .subscribe(({ status, data }) => {
-        if (status = 'success') {
+      .subscribe(({ status, data, filters }) => {
+        if ((status = "success")) {
           let receivedProducts = [];
+          this.filters = filters;
           let receivedData = { ...data };
           if (this.products.data) {
             receivedProducts = [...this.products.data, ...data.data];
@@ -64,9 +66,8 @@ export class NewProductListingComponent implements OnInit {
     const page: number = this.products.current_page + 1;
     this.loadProduct(page);
   }
-
-  removeNullItems(elm)
-  {
+ 
+  removeNullItems(elm) {
     return elm;
   }
   submit(): void {
@@ -78,17 +79,14 @@ export class NewProductListingComponent implements OnInit {
     });
     const formData = { products: submitProducts };
     this.httpService
-      .post(
-        `${env.ADMIN_API_BASE_HREF}new-products/update-multiple`,
-        formData
-      )
+      .post(`${env.ADMIN_API_BASE_HREF}new-products/update-multiple`, formData)
       .pipe(
         tap(() => (this.isProductFetching = true)),
         take(1)
       )
       .subscribe(({ status }) => {
-        if (status=='success') {
-          this.products=[];
+        if (status == "success") {
+          this.products = [];
           this.loadProduct();
         }
       });
--- src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
+++ src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-new-product-row',
-  templateUrl: './new-product-row.component.html',
-  styleUrls: ['./new-product-row.component.less']
+  selector: "app-new-product-row",
+  templateUrl: "./new-product-row.component.html",
+  styleUrls: ["./new-product-row.component.less"],
 })
 export class NewProductRowComponent implements OnInit {
-  
   @Input() product: any = {};
   @Input() index = 1;
-  constructor() { }
+  @Input() filters;
+  color_filter = [];
+  material_filter = [];
+  seating_filter = [];
+  shape_filter = [];
+  fabric_filter = [];
+
+  constructor() {}
 
   ngOnInit() {
+    this.setFilters(this.filters);
   }
 
+  setFilters(filter: any) {
+    this.color_filter = filter.Color;
+    this.material_filter = filter.Material;
+    this.shape_filter = filter.Shape;
+    this.seating_filter = filter.Seating;
+    this.fabric_filter = filter.Fabric;
+  }
 }
