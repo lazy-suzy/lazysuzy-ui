1b7c53de9ed01d2b26ebee6235ee10f50d3c87d5
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-09-16 14:43:53 +0530

    Added designer text field for cb2 and cab products and country filter and some styling changes

--- src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
+++ src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-new-product-row",
@@ -11,12 +11,14 @@ export class NewProductRowComponent implements OnInit {
   @Input() filters;
   @Input() mapping_core;
 
+  allFilters = {};
+  filterKeys = [];
   color_filter = [];
   material_filter = [];
   seating_filter = [];
   shape_filter = [];
   fabric_filter = [];
-
+  country_filter = [];
   category_detail = [];
   constructor() {}
 
@@ -27,21 +29,22 @@ export class NewProductRowComponent implements OnInit {
 
   setFilters(filter: any) {
     this.color_filter = filter.Color;
+    this.material_filter = filter.Color;
     this.material_filter = filter.Material;
     this.shape_filter = filter.Shape;
     this.seating_filter = filter.Seating;
     this.fabric_filter = filter.Fabric;
+    this.country_filter = filter.Country;
   }
-  setCategoryDetail()
-  { 
+  setCategoryDetail() {
     this.category_detail = [];
-  //  console.log(Object.keys(this.mapping_core))
-    this.product.ls_id.forEach(ls_id =>{
-      const value = this.mapping_core.filter(mapper=>mapper.LS_ID==ls_id)
-      if(value.length){
+    //  console.log(Object.keys(this.mapping_core))
+    this.product.ls_id.forEach((ls_id) => {
+      const value = this.mapping_core.filter((mapper) => mapper.LS_ID == ls_id);
+      if (value.length) {
         const category_detail_string = `${value[0].dept_name_short} > ${value[0].cat_name_short} > ${value[0].cat_sub_name}`;
         this.category_detail.push(category_detail_string);
       }
-    })
+    });
   }
 }
