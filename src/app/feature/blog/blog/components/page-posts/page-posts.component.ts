15702ee41721e3fb5c73cb1be917c5d1cae30841
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-09-28 19:15:31 +0530

    Fixed styling when viewing a blog page

--- src/app/feature/blog/blog/components/page-posts/page-posts.component.ts
+++ src/app/feature/blog/blog/components/page-posts/page-posts.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
-import {ApiService} from 'src/app/shared/services';
 import {BlogService} from 'src/app/shared/services/blog/blog.service';
 import {DomSanitizer} from '@angular/platform-browser';
 
@@ -9,18 +8,19 @@ import {DomSanitizer} from '@angular/platform-browser';
     selector: 'app-page-posts',
     templateUrl: './page-posts.component.html',
     styleUrls: ['./page-posts.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-
 })
-export class PagePostsComponent implements OnInit {
+export class PagePostsComponent implements OnInit, OnDestroy {
 
     posts$: Observable<any[]>;
     selectedPost: any;
     showLoader = true;
     postId = '';
     currentBlog: any;
-    baseUrl = 'http://wordpress.lazysuzy.com/';
-    postUrl
+    blogStylesUrl = [
+        'http://wordpress.lazysuzy.com/wp-includes/css/dist/block-library/style.min.css?ver=5.4.2',
+        'http://wordpress.lazysuzy.com/wp-content/themes/twentytwenty/style.css?ver=1.2',
+        'http://wordpress.lazysuzy.com/wp-content/themes/twentytwenty/print.css?ver=1.2'
+    ];
 
     constructor(
         private blogService: BlogService,
@@ -30,13 +30,20 @@ export class PagePostsComponent implements OnInit {
         this.route.params.subscribe(params => {
             this.postId = params.id;
         });
+        this.blogStylesUrl.forEach((url, index) => {
+            const head = document.getElementsByTagName('head')[0];
+            const link = document.createElement('link');
+            link.id = `blogUrl${index}`;
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = url;
+            head.appendChild(link);
+        });
     }
 
     ngOnInit() {
         this.showLoader = true;
-        this.postUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`http://wordpress.lazysuzy.com/index.php/2020/09/25/our-founders-home/`);
         this.posts$ = this.blogService.getPost(this.postId);
-
         this.posts$.subscribe(s => {
             this.currentBlog = s;
             let x_tags = this.currentBlog.x_tags || '';
@@ -45,5 +52,16 @@ export class PagePostsComponent implements OnInit {
             this.showLoader = false;
         });
     }
+
+    ngOnDestroy(): void {
+        console.log('destroyed');
+        const numberOfElements = this.blogStylesUrl.length - 1;
+        for (let index = 0; index < numberOfElements; index++) {
+            const id = `blogUrl${index}`;
+            const element = document.getElementById(id);
+            element.remove();
+        }
+    }
+
 
 }
