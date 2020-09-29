1c4de6ebc12e743e9205a94fe8d1ba42022a833c
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-09-29 11:02:44 +0530

    Revert "Fixed body background color and changed font family to Lato"
    This reverts commit d424b8425c1513aa22dfccb6b31e9109d3ba999a.

--- src/app/feature/blog/blog/components/page-posts/page-posts.component.ts
+++ src/app/feature/blog/blog/components/page-posts/page-posts.component.ts
@@ -1,24 +1,24 @@
-import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
 import {BlogService} from 'src/app/shared/services/blog/blog.service';
 import {DomSanitizer} from '@angular/platform-browser';
-import {UtilsService} from 'src/app/shared/services';
-import {BreakpointState} from '@angular/cdk/layout';
+import { UtilsService } from 'src/app/shared/services';
+import { BreakpointState } from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-page-posts',
     templateUrl: './page-posts.component.html',
     styleUrls: ['./page-posts.component.scss'],
-    encapsulation: ViewEncapsulation.None
 })
 export class PagePostsComponent implements OnInit, OnDestroy {
+
     posts$: Observable<any[]>;
     selectedPost: any;
     showLoader = true;
     postId = '';
     currentBlog: any;
-    isHandset = false;
+    isHandset= false;
     /**
      * Currently if we only load content without styles it looks bland, so load these additional css files
      * from word-press web-page and append to header. When this component is destroyed this files are automatically
@@ -27,7 +27,7 @@ export class PagePostsComponent implements OnInit, OnDestroy {
     blogStylesUrl = [
         'http://wordpress.lazysuzy.com/wp-includes/css/dist/block-library/style.min.css?ver=5.4.2',
         'http://wordpress.lazysuzy.com/wp-content/themes/twentytwenty/style.css?ver=1.2',
-        'http://wordpress.lazysuzy.com/wp-content/themes/twentytwenty/print.css?ver=1.2',
+        'http://wordpress.lazysuzy.com/wp-content/themes/twentytwenty/print.css?ver=1.2'
     ];
 
     constructor(
@@ -36,11 +36,9 @@ export class PagePostsComponent implements OnInit, OnDestroy {
         private sanitizer: DomSanitizer,
         private utils: UtilsService
     ) {
-        this.route.params.subscribe((params) => {
+        this.route.params.subscribe(params => {
             this.postId = params.id;
         });
-        const body = document.getElementsByTagName('body')[0];
-        body.classList.add('custom-background');
         this.loadBlogStylesToHeader();
     }
 
@@ -59,21 +57,25 @@ export class PagePostsComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.showLoader = true;
         this.posts$ = this.blogService.getPost(this.postId);
-        this.posts$.subscribe((s) => {
+        this.posts$.subscribe(s => {
             this.currentBlog = s;
             let x_tags = this.currentBlog.x_tags || '';
             x_tags = x_tags.split(',');
             this.currentBlog.x_tags = [...x_tags];
             this.showLoader = false;
         });
-        this.utils.isHandset().subscribe((handset: BreakpointState) => {
-            this.isHandset = handset.matches;
-        });
+        this.utils.isHandset().subscribe(
+            (handset: BreakpointState) => {
+                this.isHandset = handset.matches;
+              }
+        )
     }
 
     ngOnDestroy(): void {
+        console.log('destroyed');
         this.removeBlogStylesFromHeader();
     }
+
 
     private removeBlogStylesFromHeader() {
         const numberOfElements = this.blogStylesUrl.length - 1;
@@ -82,7 +84,5 @@ export class PagePostsComponent implements OnInit, OnDestroy {
             const element = document.getElementById(id);
             element.remove();
         }
-        const body = document.getElementsByTagName('body')[0];
-        body.classList.remove('custom-background');
     }
 }
