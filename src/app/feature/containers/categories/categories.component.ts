aeefeafc9c3959028d221757664d4fe92f36f337
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-11-07 17:58:35 +0530

    Add seo for department page

--- src/app/feature/containers/categories/categories.component.ts
+++ src/app/feature/containers/categories/categories.component.ts
@@ -1,51 +1,64 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { IAllCategory } from '../../../shared/models/all-department.interface';
-import { ApiService } from './../../../shared/services';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {IAllCategory} from '../../../shared/models/all-department.interface';
+import {ApiService, SeoService} from './../../../shared/services';
+import {ActivatedRoute, Router} from '@angular/router';
+import MetaData from '../../../shared/services/seo/meta-data-model';
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.less']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.less']
 })
 export class CategoriesComponent implements OnInit {
-  routeSubscription: Subscription;
-  categorySubscription: Subscription;
-  department: string;
-  categories: IAllCategory[];
-  constructor(
-    private apiService: ApiService,
-    private router: Router,
-    private activeRoute: ActivatedRoute
-  ) {}
+    routeSubscription: Subscription;
+    categorySubscription: Subscription;
+    department: string;
+    categories: IAllCategory[];
 
-  ngOnInit(): void {
-    this.getParams();
-    this.routeSubscription = this.activeRoute.params.subscribe(
-      (routeParams) => {
-        this.department = routeParams.department;
+    constructor(
+        private apiService: ApiService,
+        private router: Router,
+        private activeRoute: ActivatedRoute,
+        private seoService: SeoService
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.getParams();
+        this.routeSubscription = this.activeRoute.params.subscribe(
+            (routeParams) => {
+                this.department = routeParams.department;
+                this.getCategories();
+            }
+        );
         this.getCategories();
-      }
-    );
-    this.getCategories();
-  }
+    }
 
-  onDestroy(): void {
-    this.routeSubscription.unsubscribe();
-    this.categorySubscription.unsubscribe();
-  }
+    onDestroy(): void {
+        this.routeSubscription.unsubscribe();
+        this.categorySubscription.unsubscribe();
+    }
 
-  getParams(): void {
-    const urlParams: string[] = this.router.url.split('/').slice(1);
-    this.department = urlParams[1];
-  }
+    getParams(): void {
+        const urlParams: string[] = this.router.url.split('/').slice(1);
+        this.department = urlParams[1];
+    }
 
-  getCategories() {
-    this.categorySubscription = this.apiService
-      .getCategories(this.department)
-      .subscribe((payload: any) => {
-        this.categories = payload.categories;
-      });
-  }
+    getCategories() {
+        this.categorySubscription = this.apiService
+            .getCategories(this.department)
+            .subscribe((payload: any) => {
+                this.categories = payload.categories;
+                this.setMetaTags(payload);
+            });
+    }
+
+    setMetaTags(department) {
+        const metaData: MetaData = {};
+        metaData.title = `Search from hundreds of ${department.department_long} products of top brands at once.`;
+        metaData.description = `Search from hundreds of ${department.department_long} products of top brands at once.Add to your room designs with your own design boards.`;
+        metaData.image = department.department_image;
+        this.seoService.setMetaTags(metaData);
+    }
 }
