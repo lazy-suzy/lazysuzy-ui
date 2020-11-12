4900f30831f82ce08c3d204ebd9a40335ff0386c
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-11-12 18:34:43 +0530

    Fix git

--- src/app/feature/components/variations/variations.component.ts
+++ src/app/feature/components/variations/variations.component.ts
@@ -145,7 +145,6 @@ export class VariationsComponent implements OnInit {
                 this.matDialogUtils.openVariationDialog(variation.variation_sku);
             }
         } else {
-            this.clearVariations(true);
             this.selectedSwatch = {
                 image: variation.image,
                 swatch_image: variation.swatch_image,
@@ -425,18 +424,7 @@ export class VariationsComponent implements OnInit {
      */
     clearVariations() {
         this.clearSelection.emit(true);
-        if (singleSelect) {
-            const keys = Object.keys(this.inputSelections).filter(key => this.inputSelections[key].select_type === 'single_select');
-            for (const key of keys) {
-                this.selections.hasOwnProperty(key);
-                {
-                    delete this.selections[key];
-                }
-            }
-        } else {
-            this.selections = {};
-        }
-
+        this.selections = {};
         this.selectedIndex = null;
         this.setPrice.emit('');
         this.setImage.emit('');
