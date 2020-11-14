addb0df5531636a39a3b2c62ea2e04b8483e34eb
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-11-14 19:03:31 +0530

    added new product dashboard

--- src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
+++ src/app/modules/new-products-dashboard/new-product-row/new-product-row.component.ts
@@ -1,52 +1,105 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {Component, Input, OnInit} from '@angular/core';
+
+interface ImageItem {
+    index: number;
+    image: string;
+}
 
 @Component({
-  selector: "app-new-product-row",
-  templateUrl: "./new-product-row.component.html",
-  styleUrls: ["./new-product-row.component.less"],
+    selector: 'app-new-product-row',
+    templateUrl: './new-product-row.component.html',
+    styleUrls: ['./new-product-row.component.less'],
 })
 export class NewProductRowComponent implements OnInit {
-  @Input() product: any = {};
-  @Input() index = 1;
-  @Input() filters;
-  @Input() mapping_core;
+    @Input() product: any = {};
+    @Input() index = 1;
+    @Input() filters;
+    @Input() mapping_core;
 
-  allFilters = {};
-  filterKeys = [];
-  color_filter = [];
-  material_filter = [];
-  seating_filter = [];
-  shape_filter = [];
-  fabric_filter = [];
-  country_filter = [];
-  category_detail = [];
-  constructor() {}
+    allFilters = {};
+    filterKeys = [];
+    color_filter = [];
+    material_filter = [];
+    seating_filter = [];
+    shape_filter = [];
+    fabric_filter = [];
+    country_filter = [];
+    category_detail = [];
 
-  ngOnInit() {
-    this.setFilters(this.filters);
-    this.setCategoryDetail();
-  }
+    xImagePrimary = [];
+    xImageSecondary = [];
 
-  setFilters(filter: any) {
-    this.color_filter = filter.Color;
-    this.material_filter = filter.Color;
-    this.material_filter = filter.Material;
-    this.shape_filter = filter.Shape;
-    this.seating_filter = filter.Seating;
-    this.fabric_filter = filter.Fabric;
-    this.country_filter = filter.Country;
-  }
-  setCategoryDetail() {
-    this.category_detail = [];
-    //  console.log(Object.keys(this.mapping_core))
-    if(this.product.ls_id){
-      this.product.ls_id.forEach((ls_id) => {
-        const value = this.mapping_core.filter((mapper) => mapper.LS_ID == ls_id);
-        if (value.length) {
-          const category_detail_string = `${value[0].dept_name_short} > ${value[0].cat_name_short} > ${value[0].cat_sub_name}`;
-          this.category_detail.push(category_detail_string);
-        }
-      });
+    constructor() {
     }
-  }
+
+    ngOnInit() {
+        this.setFilters(this.filters);
+        this.setCategoryDetail();
+    }
+
+    setFilters(filter: any) {
+        this.color_filter = filter.Color;
+        this.material_filter = filter.Color;
+        this.material_filter = filter.Material;
+        this.shape_filter = filter.Shape;
+        this.seating_filter = filter.Seating;
+        this.fabric_filter = filter.Fabric;
+        this.country_filter = filter.Country;
+    }
+
+    setCategoryDetail() {
+        this.category_detail = [];
+        //  console.log(Object.keys(this.mapping_core))
+        if (this.product.ls_id) {
+            this.product.ls_id.forEach((ls_id) => {
+                const value = this.mapping_core.filter((mapper) => mapper.LS_ID == ls_id);
+                if (value.length) {
+                    const category_detail_string = `${value[0].dept_name_short} > ${value[0].cat_name_short} > ${value[0].cat_sub_name}`;
+                    this.category_detail.push(category_detail_string);
+                }
+            });
+        }
+    }
+
+    updateImage(index: number, image: string, $event) {
+        const imageItem: ImageItem = {
+            index,
+            image
+        };
+        if ($event.value === 'primary') {
+            this.checkSecondaryImage(imageItem);
+            this.xImagePrimary.push(imageItem);
+        } else {
+            this.checkPrimaryImage(imageItem);
+            this.xImageSecondary.push(imageItem);
+        }
+        this.product.image_xbg_select_primary = this.xImagePrimary.map(this.imageOnly).join(',');
+        this.product.image_xbg_select_secondary = this.xImageSecondary.map(this.imageOnly).join(',');
+    }
+
+    imageOnly(imageItem: ImageItem) {
+        return imageItem.image;
+    }
+
+    checkPrimaryImage(imageItem) {
+        const itemIndex = this.xImagePrimary.findIndex((value: ImageItem) => value.index === imageItem.index);
+        if (itemIndex !== -1) {
+            this.xImagePrimary.splice(itemIndex, 1);
+        }
+    }
+
+    checkSecondaryImage(imageItem) {
+        const itemIndex = this.xImageSecondary.findIndex((value: ImageItem) => value.index === imageItem.index);
+        if (itemIndex !== -1) {
+            this.xImageSecondary.splice(itemIndex, 1);
+        }
+    }
+
+    private updatePrimaryXImage(index, image) {
+        const imageItem = {
+            index,
+            image
+        };
+        this.xImageSecondary.push(imageItem);
+    }
 }
