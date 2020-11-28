1d7f126841e01bf7e286bc819929253c3c0d1193
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-11-28 11:53:16 +0530

    Fix negative mobile pprice

--- src/app/feature/components/product-mobile/product-mobile.component.ts
+++ src/app/feature/components/product-mobile/product-mobile.component.ts
@@ -1,62 +1,64 @@
-import { ProductDetailsComponent } from './../product-details/product-details.component';
-import { IProductPayload } from './../../../shared/models';
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ApiService } from 'src/app/shared/services';
+import {ProductDetailsComponent} from './../product-details/product-details.component';
+import {IProductPayload} from './../../../shared/models';
+import {Component, OnInit, Input} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ApiService} from 'src/app/shared/services';
 
 @Component({
-  selector: 'app-product-mobile',
-  templateUrl: './product-mobile.component.html',
-  styleUrls: ['./product-mobile.component.less']
+    selector: 'app-product-mobile',
+    templateUrl: './product-mobile.component.html',
+    styleUrls: ['./product-mobile.component.less']
 })
 export class ProductMobileComponent implements OnInit {
-  @Input() product: IProductPayload;
-  @Input() productsInRow: number;
+    @Input() product: IProductPayload;
+    @Input() productsInRow: number;
 
-  isVariationImageVisible = false;
-  priceObject = {
-    isPrice:0,
-    wasPrice:0
-  };
-  isDiscounted = false;
-  isRange = false;
-  constructor(private apiService: ApiService) {}
+    isVariationImageVisible = false;
+    priceObject = {
+        isPrice: 0,
+        wasPrice: 0
+    };
+    isDiscounted = false;
+    isRange = false;
 
-  ngOnInit() {
-    this.formatPriceText(this.product);
-  }
+    constructor(private apiService: ApiService) {
+    }
 
-  formatPriceText(product){
-    let {is_price, was_price} = product;
-    is_price = is_price.split('-');
-    was_price = was_price.split('-');
-    if(is_price.length>1){
-        this.isRange = true;
+    ngOnInit() {
+        this.formatPriceText(this.product);
     }
-    this.priceObject.isPrice = Number(is_price[0]);
-    this.isDiscounted = was_price && was_price[0] > is_price[0];
-    if(this.isDiscounted){
-      this.priceObject.wasPrice = Number(was_price[0]);
-    }
-  }
-  wishlistProduct(sku, mark, event) {
-    event.stopPropagation();
-    this.apiService
-      .wishlistProduct(sku, mark, true)
-      .subscribe((payload: any) => {
-        this.product.wishlisted = mark;
-      });
-  }
 
-  renderPrice(price:number):number|string{
-    const quotient = Math.floor(price);
-    let remainder = Number((price - quotient).toPrecision(2));
-    if(remainder == 0){
-      return quotient;
+    formatPriceText(product) {
+        let {is_price, was_price} = product;
+        is_price = is_price.split('-');
+        was_price = was_price.split('-');
+        if (is_price.length > 1) {
+            this.isRange = true;
+        }
+        this.priceObject.isPrice = Number(is_price[0]);
+        this.isDiscounted = was_price && Number(was_price[0]) > Number(is_price[0]);
+        if (this.isDiscounted) {
+            this.priceObject.wasPrice = Number(was_price[0]);
+        }
     }
-    else{
-      return  price.toFixed(2);
+
+    wishlistProduct(sku, mark, event) {
+        event.stopPropagation();
+        this.apiService
+            .wishlistProduct(sku, mark, true)
+            .subscribe((payload: any) => {
+                this.product.wishlisted = mark;
+            });
     }
-    return 0;
-  }
+
+    renderPrice(price: number): number | string {
+        const quotient = Math.floor(price);
+        let remainder = Number((price - quotient).toPrecision(2));
+        if (remainder == 0) {
+            return quotient;
+        } else {
+            return price.toFixed(2);
+        }
+        return 0;
+    }
 }
