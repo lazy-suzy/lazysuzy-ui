7882b0bb4600b94b1a7ad4ee1abb07a5527bbf06
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2020-12-08 14:50:02 +0530

    Fix carousel and brand

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ApiService } from './../../../shared/services';
+import {Component, OnInit, Input} from '@angular/core';
+import {ApiService} from './../../../shared/services';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.less']
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.less']
 })
 export class BannerComponent implements OnInit {
-  bannerData: any;
-  isBanner = true;
-  @Input() isHandset = false;
+    bannerData: any;
+    isBanner = true;
+    @Input() isHandset = false;
 
-  constructor(
-    private apiService: ApiService,
-  ) {}
+    constructor(
+        private apiService: ApiService,
+    ) {
+    }
 
-  ngOnInit() {
-    this.apiService.bannerData().subscribe((res) => {
-      this.bannerData = res;
-    });
-  }
+    ngOnInit() {
+        this.apiService.bannerData().subscribe((res: any[]) => {
+            this.bannerData = res.filter(value => value.position === 'primary');
+        });
+    }
 }
