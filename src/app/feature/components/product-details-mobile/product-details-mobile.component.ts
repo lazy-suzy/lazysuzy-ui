6981f80430fc7f60a1bbaf2f694f9878093dcc4d
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-25 00:23:37 +0530

    review mobile

--- src/app/feature/components/product-details-mobile/product-details-mobile.component.ts
+++ src/app/feature/components/product-details-mobile/product-details-mobile.component.ts
@@ -551,7 +551,7 @@ export class ProductDetailsMobileComponent implements OnInit {
     }
 
     goToReview(sku) {
-        window.location.href = './product/review/' + sku;
-        //this.router.navigateByUrl('/product/review/',sku)
+       // window.location.href = './product/review/' + sku;
+        this.router.navigateByUrl('/product/review/',sku)
     }
 }
