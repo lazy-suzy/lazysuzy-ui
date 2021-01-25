4ce3df22d1b5841fb3274834595f83ea10e0f8ca
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2021-01-25 19:58:51 +0530

    Add reviews on product-details-mobile page

---
+++ src/app/feature/components/product-details-mobile/review-mobile/review-mobile.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Gallery, GalleryItem, ImageItem} from '@ngx-gallery/core';
+import {Lightbox} from '@ngx-gallery/lightbox';
+
+@Component({
+  selector: 'app-review-mobile',
+  templateUrl: './review-mobile.component.html',
+  styleUrls: ['./review-mobile.component.less']
+})
+export class ReviewMobileComponent implements OnInit {
+  @Input() review: any;
+  @Input() galleryId: any;
+  items: GalleryItem[] | any;
+  galleryRef;
+  constructor(
+      public gallery: Gallery,
+      public lightbox: Lightbox,
+  ) { }
+
+  ngOnInit() {
+    this.galleryRef = this.gallery.ref(this.galleryId);
+    if (this.review.review_images) {
+      const images = this.review.review_images.split(',').filter(value => value);
+      this.createGalleryItems(images);
+    }
+  }
+  createGalleryItems(items: any[]) {
+    this.items = items.map((item) => new ImageItem({src: item, thumb: item}));
+    const lightboxGalleryRef = this.gallery.ref(this.galleryRef);
+    // 3. Load the items into the lightbox
+    lightboxGalleryRef.load(this.items);
+    // this.galleryRef.load(this.items);
+  }
+
+}
